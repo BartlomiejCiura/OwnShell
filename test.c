8546b4d5412dff08c3c7cb29ee0820b5141edd8c
BartoszDabek <bartek50600@wp.pl>
2017-04-27 20:19:57 +0200

    added built in commands + make some clean in code

--- test.c
+++ test.c
@@ -7,15 +7,25 @@
 
 void parseCmd(char* cmd, char** params);
 int executeCmd(char** params);
-int go_cd(char** params) {
-	if (params[1] == NULL) {
-		fprintf(stderr, "ERROR: expected argument to \"cd\"\n");
-	} else {
-		if (chdir(params[1]) != 0) {
-			perror("ERROR");
-		}
-	}
-	return 1;
+int go_cd(char **args);
+int go_help(char **args);
+int go_exit(char **args);
+int is_build_command(char **args); 
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &go_cd,
+  &go_help,
+  &go_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
 }
 
 #define MAX_COMMAND_LENGTH 100
@@ -23,53 +33,47 @@ int go_cd(char** params) {
 
 int main()
 {
-    char cmd[MAX_COMMAND_LENGTH + 1];
-    char* params[MAX_NUMBER_OF_PARAMS + 1];
+	char cmd[MAX_COMMAND_LENGTH + 1];
+	char* params[MAX_NUMBER_OF_PARAMS + 1];
+	
+	printf("Type 'help' to check built in commands\n");
 
-    while(1) {
-        // Print command prompt
-        char* username = getenv("USER");
-        printf("%s@shell: ", username);
+	while(1) {
+		// Print command prompt
+		char* username = getenv("USER");
+		printf("%s@shell: ", username);
 
-        // Read command from standard input, exit on Ctrl+D
-        if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
+		// Read command from standard input, exit on Ctrl+D
+		if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
 
-        // Remove trailing newline character, if any
-        if(cmd[strlen(cmd)-1] == '\n') {
-            cmd[strlen(cmd)-1] = '\0';
-        }
+		// Remove trailing newline character, if any
+		if(cmd[strlen(cmd)-1] == '\n') {
+			cmd[strlen(cmd)-1] = '\0';
+		}
 
-        // Split cmd into array of parameters
-        parseCmd(cmd, params);
-        // Exit?
-        if(strcmp(params[0], "exit") == 0) break;
+		// Split cmd into array of parameters
+		parseCmd(cmd, params);
 
-        // Execute command
-			if(strcmp(params[0], "cd") == 0) {
-				go_cd(params);
-			}
-         else if(executeCmd(params) == 0) break;
-    }
-
-    return 0;
+		if(is_build_command(params) == 0) break;
+		else if(executeCmd(params) == 0) break;
+}
+return 0;
 }
 
 // Split cmd into array of parameters
-void parseCmd(char* cmd, char** params)
-{       
+void parseCmd(char* cmd, char** params) {       
     for(int i = 0; i < MAX_NUMBER_OF_PARAMS; i++) {
         params[i] = strsep(&cmd, " ");
         if(params[i] == NULL) break;
     }
 }
 
-int executeCmd(char** params)
-{
+int executeCmd(char** params) {
     // Fork process
     pid_t pid = fork();
 
     // Error
-    if (pid == -1) {
+	 if (pid == -1) {
         char* error = strerror(errno);
         printf("fork: %s\n", error);
         return 1;
@@ -81,8 +85,8 @@ int executeCmd(char** params)
 	     execvp(params[0], params);  
 
         // Error occurred
-        char* error = strerror(errno);
-        printf("bledzik: %s: %s\n", params[0], error);
+   //     char* error = strerror(errno);
+   //     printf("bledzik: %s: %s\n", params[0], error);
         return 0;
     }
 
@@ -93,4 +97,39 @@ int executeCmd(char** params)
         waitpid(pid, &childStatus, 0);
         return 1;
     }
+}
+
+int go_cd(char** params) {
+	if (params[1] == NULL) {
+		fprintf(stderr, "ERROR: expected argument to \"cd\"\n");
+	} else {
+		if (chdir(params[1]) != 0) {
+			perror("ERROR");
+		}
+	}
+	return 1;
+}
+
+int go_help(char **args) {
+	int i;
+	printf("The following commands are built in:\n");
+
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		printf("--> %s\n", builtin_str[i]);
+  	}
+  	return 1;
+}
+
+int go_exit(char **args) {
+	return 0;
+}
+
+int is_build_command(char **args) {
+	int i;
+	for (i = 0; i < lsh_num_builtins(); i++) {
+   	if (strcmp(args[0], builtin_str[i]) == 0) {
+   	   return (*builtin_func[i])(args);
+    	}
+  	}	
+	return 1;
 }
